a9eb3bb851997f2bb4dfe58d830eb493b824899c
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-02-28 15:48:05 +0600

    connect js css and html

--- script.js
+++ script.js
@@ -31,3 +31,48 @@ exampleBtn.remove();
 sayHelloBtn.addEventListener("click", function(event){
   alert("Hello");
 })
+//создать кнопку добавить его к элементу боди и изменить текст этой кнопки
+let example = document.querySelector ("#example");
+example.textContent = "example";
+example.style.padding = "10px";
+document.body.append(example);
+
+example.addEventListener("click", function() {
+  example.remove()
+})
+// создать список и добавить к боди
+
+let list = document.createElement("ul");
+document.body.append(list);
+for (let i =1; i <= 10; i ++) {
+  let listEl = document.createElement ("li")
+  list.append(listEl);
+}
+
+let list1 = ["Apples", "Pears", "Oranges", "Peaches"];
+let list2 = document.createElement("ul");
+document.body.append(list2)
+for ( let a = 0; a < list1.length; a ++) {
+  let listItems = document.createElement ("li");
+  listItems.textContent = list1[a];
+  list2.append(listItems);
+}
+
+let par = document.createElement("p")
+let elParrent = document.querySelector("#example1") 
+let elements = elParrent.querySelectorAll("li")
+elements.forEach((el) => {
+  par.innerText = par.innerText + el.innerText + " "
+})
+document.body.append(par)
+
+// let paragraph = document.createElement("p")
+// let eleParrent = document.querySelector("#example2")
+// let element = eleParrent.querySelector("li")
+// element.forEach((el) => {
+//   paragraph.innerText = paragraph.innerText + el.innerText + " "
+// } )
+// document.body.prepend(paragraph)
+
+// свойства объекта можнопоменять но не сам объект
+// использовать константу как можно везде
